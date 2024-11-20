03e2ef5b7eca407fddf7542ef8597328bfd6cb10
webcreation-dev <adjilan2403@gmail.com>
2024-11-20 21:22:57 +0100

    creating order item entity and handling many to man relations

---
+++ src/domain/orders/entities/order-item.entity.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, ManyToOne, PrimaryColumn } from 'typeorm';
+import { Order } from './order.entity';
+import { Product } from 'products/entities/product.entity';
+
+@Entity()
+export class OrderItem {
+  @PrimaryColumn()
+  orderId: number;
+
+  @PrimaryColumn()
+  productId: number;
+
+  @Column()
+  quantity: number;
+
+  @Column({ type: 'decimal', precision: 6, scale: 2 })
+  price: number;
+
+  @ManyToOne(() => Order, (order) => order.items, { onDelete: 'CASCADE' })
+  order: Order;
+
+  @ManyToOne(() => Product, (product) => product.items)
+  product: Product;
+}
