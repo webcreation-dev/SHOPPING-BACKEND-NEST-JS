1ae42f745fae36a507620614b38b5600c4cf09ce
webcreation-dev <adjilan2403@gmail.com>
2024-11-21 11:04:57 +0100

    implementing order logic and saving with cascade

--- src/domain/orders/orders.controller.ts
+++ src/domain/orders/orders.controller.ts
@@ -3,14 +3,12 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
   Query,
 } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
 import { IdDto } from 'common/dto/id.dto';
 import { PaginationDto } from 'common/dto/pagination.dto';
 
@@ -31,11 +29,6 @@ export class OrdersController {
   @Get(':id')
   findOne(@Param() { id }: IdDto) {
     return this.ordersService.findOne(id);
-  }
-
-  @Patch(':id')
-  update(@Param() { id }: IdDto, @Body() updateOrderDto: UpdateOrderDto) {
-    return this.ordersService.update(id, updateOrderDto);
   }
 
   @Delete(':id')
--- src/domain/orders/orders.service.ts
+++ src/domain/orders/orders.service.ts
@@ -1,21 +1,36 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
 import { DEFAULT_PAGE_SIZE } from 'common/util/common.constants';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
 import { Repository } from 'typeorm';
 import { PaginationDto } from 'common/dto/pagination.dto';
+import { OrderItemDto } from './dto/order-item.dto';
+import { Product } from 'products/entities/product.entity';
+import { OrderItem } from './entities/order-item.entity';
 
 @Injectable()
 export class OrdersService {
   constructor(
     @InjectRepository(Order)
     private readonly ordersRepository: Repository<Order>,
+    @InjectRepository(OrderItem)
+    private readonly orderItemsRepository: Repository<OrderItem>,
+    @InjectRepository(Product)
+    private readonly productsRepository: Repository<Product>,
   ) {}
 
-  create(createOrderDto: CreateOrderDto) {
-    const order = this.ordersRepository.create(createOrderDto);
+  async create(createOrderDto: CreateOrderDto) {
+    const { items } = createOrderDto;
+
+    const itemsWithPrice = await Promise.all(
+      items.map((item) => this.createOrderItemWithPrice(item)),
+    );
+
+    const order = this.ordersRepository.create({
+      ...createOrderDto,
+      items: itemsWithPrice,
+    });
     return this.ordersRepository.save(order);
   }
 
@@ -29,26 +44,40 @@ export class OrdersService {
   }
 
   async findOne(id: number) {
-    const order = await this.ordersRepository.findOneBy({ id });
+    const order = await this.ordersRepository.findOne({
+      where: { id },
+      relations: {
+        items: {
+          product: true,
+        },
+        customer: true,
+        payment: true,
+      },
+    });
     if (!order) {
       throw new NotFoundException('Order not found');
     }
     return order;
   }
 
-  async update(id: number, updateOrderDto: UpdateOrderDto) {
-    const order = await this.ordersRepository.preload({
-      id,
-      ...updateOrderDto,
-    });
-    if (!order) {
-      throw new NotFoundException('Order not found');
-    }
-    return this.ordersRepository.save(order);
-  }
-
   async remove(id: number) {
     const order = await this.findOne(id);
     return this.ordersRepository.remove(order);
+  }
+
+  private async createOrderItemWithPrice(orderItemDto: OrderItemDto) {
+    const { id } = orderItemDto.product;
+
+    const product = await this.productsRepository.findOneBy({ id });
+    if (!product) {
+      throw new NotFoundException('Product not found');
+    }
+    const { price } = product;
+
+    const orderItem = this.orderItemsRepository.create({
+      ...orderItemDto,
+      price,
+    });
+    return orderItem;
   }
 }
