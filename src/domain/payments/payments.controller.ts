ee50ee65a323fb5976395e34af794c44ec08d422
webcreation-dev <adjilan2403@gmail.com>
2024-11-21 13:40:44 +0100

    implementing commit logic

--- src/domain/payments/payments.controller.ts
+++ src/domain/payments/payments.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Post } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
+import { IdDto } from 'common/dto/id.dto';
 
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
+
+  @Post(':id')
+  payOrder(@Param() { id }: IdDto) {
+    return this.paymentsService.payOrder(id);
+  }
 }
--- src/domain/payments/payments.module.ts
+++ src/domain/payments/payments.module.ts
@@ -3,9 +3,10 @@ import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 import { Payment } from './entities/payment.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from 'orders/entities/order.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment])],
+  imports: [TypeOrmModule.forFeature([Payment, Order])],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
