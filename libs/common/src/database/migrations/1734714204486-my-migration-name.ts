1f0c700f4275bda489f677851a34f6aa319fa891
webcreation-dev <adjilan2403@gmail.com>
2024-12-20 09:10:30 -0800

    fix bugs

---
+++ libs/common/src/database/migrations/1734714204486-my-migration-name.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class MyMigrationName1734714204486 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
