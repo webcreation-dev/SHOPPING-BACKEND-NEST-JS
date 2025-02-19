3aafdfb74c25c5933959ace128ba21db0efbaba6
webcreation-dev <adjilan2403@gmail.com>
2025-02-19 10:38:08 -0800

    deploying on heroku

--- libs/common/src/database/data-source.ts
+++ libs/common/src/database/data-source.ts
@@ -14,10 +14,10 @@ const dataSource = new DataSource({
     'dist/src/features/properties/entities/*.js',
   ],
   migrations: ['dist/libs/common/src/database/migrations/*.js'],
+  ssl: true,
   extra: {
     ssl: {
       rejectUnauthorized: false,
-      require: true,
     },
   },
 });
