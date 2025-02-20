5decd6eeffb9cd09f1953da937c57f99ec10c57c
webcreation-dev <adjilan2403@gmail.com>
2025-02-20 02:50:35 -0800

    add redoc doc online

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(helmet());
 
+  // Configuration Swagger
   const config = new DocumentBuilder()
     .setTitle('LOCAPAY BACKEND')
     .setDescription('Documentation for the LOCAPAY API')
@@ -15,7 +17,9 @@ async function bootstrap() {
     .addSecurityRequirements('bearer')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
+  writeFileSync('./openapi.json', JSON.stringify(document, null, 2));
   SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
