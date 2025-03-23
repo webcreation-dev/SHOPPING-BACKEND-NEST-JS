9eb2ec093e9798fe7f28439f7b41c3cfc8023c58
webcreation-dev <adjilan2403@gmail.com>
2025-03-23 08:47:16 -0700

    fix bug users

--- src/main.ts
+++ src/main.ts
@@ -36,6 +36,19 @@ async function bootstrap() {
 
   app.use('/upload', express.static(uploadsPath));
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    );
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
+  });
+
   const document = SwaggerModule.createDocument(app, config);
   writeFileSync('./openapi.json', JSON.stringify(document, null, 2));
   SwaggerModule.setup('api', app, document);
