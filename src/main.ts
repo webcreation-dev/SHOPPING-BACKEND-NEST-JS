33df875b5800f21278570ee00faf43c8ed933dab
webcreation-dev <adjilan2403@gmail.com>
2025-05-09 20:05:26 +0100

    add url frontend deploy

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
     origin: [
       'https://locapay-manager.vercel.app',
       'http://localhost:5173',
+      'http://185.218.125.120:8085',
       'https://locapay-8f958cc17518.herokuapp.com',
       'https://www.locapay.app',
       'http://localhost:8080',
